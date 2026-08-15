9fb6c47490db7a7ffccccd27ab83c0dfb90126ff
Jovan Ong <jovanong04@yahoo.com.sg>
2026-08-15 13:41:49 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
         String banner = " _   _  _____     ___    \n"
                 + "| \\ | |/ _ \\ \\   / / \\   \n"
                 + "|  \\| | | | \\ \\ / / _ \\  \n"
@@ -9,10 +12,33 @@ public class Duke {
         System.out.println(banner);
         System.out.println("Hi there Big Boss! I am your assistant NOVA");
         System.out.println("Your wish is my command!");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
 
+        System.out.println("____________________________________________________________");
+        System.out.println("Type your Command Below");
+        System.out.println("Or");
+        System.out.println("Type -bye- to exit");
+
+
+
+
+        //creating the scanner
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye BossSe. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println("Let Nova help you with this task: -" + input);
+            System.out.println(line);
+
+        }
+        scanner.close();
     }
 
 }
