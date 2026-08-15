25a073542d57f7180081d1d39b182878d3913be9
Jovan Ong <jovanong04@yahoo.com.sg>
2026-08-15 16:42:43 +0800

    task 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,9 +20,20 @@ public class Duke {
         System.out.println("Or");
         System.out.println("Type -list- to see your history of commands");
 
+        System.out.println("____________________________________________________________");
+        System.out.println("Type -mark [task_number]- to mark tasks");
+        System.out.println("Or");
+        System.out.println("Type -unmark [task_number]- to unmark previously marked tasks");
+
+
+
+
         // Create storage
         String[] tasks = new String[100];
         int taskCount = 0;
+
+        //for mark
+        boolean[] isDone = new boolean[100];
 
 
         //creating the scanner
@@ -41,16 +52,45 @@ public class Duke {
                 System.out.println(line);
                 System.out.println("Your Lists:");
                 for(int i = 0; i < tasks.length; i++){
+                    String status;
                     if (tasks[i]==null){
                         break;
                     }
-                    System.out.println("Task "+ (i+1) + ". : " +tasks[i]);
+                    if (isDone[i]){
+                        status = "[X]";
+                    }
+                    else{ status = "[ ]";}
+
+                    System.out.println("Task "+ (i+1) + ". "+ status+" "+ tasks[i]);
                     System.out.println("||");
                 }
                 System.out.println(line);
 
             }
 
+            //MARK feature
+            else if (input.startsWith("mark ")){
+                int Task_number = Integer.parseInt(input.substring(5));
+                int index = Task_number - 1;
+                isDone[index] = true;
+                System.out.println(line);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + tasks[index]);
+                System.out.println(line);
+            }
+
+            //UNMARK feature
+            else if (input.startsWith("unmark ")){
+                int Task_number = Integer.parseInt(input.substring(5));
+                int index = Task_number - 1;
+                isDone[index] = false;
+                System.out.println(line);
+                System.out.println("Ok! I've unmarked this task:");
+                System.out.println("[ ] " + tasks[index]);
+                System.out.println(line);
+            }
+
+            //Adding tasks
             else{
                 tasks[taskCount] = input;
                 taskCount++;
