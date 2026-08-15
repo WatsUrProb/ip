60aa60a330272dd7a179009349a57a206ee11845
Jovan Ong <jovanong04@yahoo.com.sg>
2026-08-15 13:25:10 +0800

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String banner = " _   _  _____     ___    \n"
+                + "| \\ | |/ _ \\ \\   / / \\   \n"
+                + "|  \\| | | | \\ \\ / / _ \\  \n"
+                + "| |\\  | |_| |\\ V / ___ \\ \n"
+                + "|_| \\_|\\___/  \\_/_/   \\_\\\n";
+        System.out.println("____________________________________________________________");
         System.out.println(banner);
+        System.out.println("Hi there Big Boss! I am your assistant NOVA");
+        System.out.println("Your wish is my command!");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
     }
+
 }
