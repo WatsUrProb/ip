a9d177c2dbd852fdf3d52aba3dcbac18a461ecbc
Jovan Ong <jovanong04@yahoo.com.sg>
2026-08-19 17:24:24 +0800

    Alter Deadline.java

--- src/main/java/tasks/Deadline.java
+++ src/main/java/tasks/Deadline.java
@@ -1,16 +1,20 @@
 package tasks;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task {
 
-    protected String by;
-    public Deadline(String description, String by) {
+    private LocalDateTime by;
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]"+super.toString()+" (by: "+this.by + " )";
+        DateTimeFormatter outputFormat = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mm a");
+
+        return "[D]"+super.toString()+" (by: "+by.format(outputFormat) +")";
     }
 
     @Override
