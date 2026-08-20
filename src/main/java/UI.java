514dbfb766398d22bf5eb3e4493bb0d257adb058
Jovan Ong <jovanong04@yahoo.com.sg>
2026-08-20 11:18:01 +0800

    new parser class to handle responses to exception cases

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -11,17 +11,40 @@ public class UI {
     }
 
     public void showWelcome() {
+        String line = "____________________________________________________________";
         String banner = " _   _  _____     ___    \n"
                 + "| \\ | |/ _ \\ \\   / / \\   \n"
                 + "|  \\| | | | \\ \\ / / _ \\  \n"
                 + "| |\\  | |_| |\\ V / ___ \\ \n"
                 + "|_| \\_|\\___/  \\_/_/   \\_\\\n";
-
-        System.out.println(line);
+        System.out.println("____________________________________________________________");
         System.out.println(banner);
         System.out.println("Hi there Big Boss! I am your assistant NOVA");
         System.out.println("Your wish is my command!");
-        System.out.println(line);
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Type your Command Below");
+        System.out.println("____________________________________________________________");
+
+        String s = "dd/hh/yyyy";
+        System.out.println("deadline [-description-] /by " + s);
+        System.out.println("event [-description-] /from dd/hh/yyyy /to dd/hh/yyyy");
+        System.out.println("todo [-description-]");
+        System.out.println("____________________________________________________________");
+
+        System.out.println("Or");
+        System.out.println("Type -bye- to exit");
+        System.out.println("Or");
+        System.out.println("Type -list- to see your history of commands");
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Type -mark [task_number]- to mark tasks");
+        System.out.println("Or");
+        System.out.println("Type -unmark [task_number]- to unmark previously marked tasks");
+        System.out.println(" ");
+        System.out.println("____________________________________________________________");
+        System.out.println("Type -delete [task_number]- to remove tasks");
+        System.out.println("____________________________________________________________");
     }
 
     public String readCommand() {
